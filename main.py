b133044219d30b7a7878247c95edaa4ef3aa5be1
Hakan Orak <hakan.orak@hotmail.fr>
2022-04-27 14:55:59 +0200

    feat(standardScaler): mise en place du standardScaler sur les variables numeriques

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+import numpy
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import warnings
 import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.datasets import load_iris
+from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder, OrdinalEncoder
 
+
 def init():
-    global df,test_ID,train_ID
+    global df, test_ID, train_ID
 
     train = pd.read_csv('data/train.csv')
     test = pd.read_csv('data/test.csv')
@@ -16,7 +20,8 @@ def init():
 
     test_ID = test['PassengerId']
     train_ID = train['PassengerId']
-    df = pd.concat([train,test],axis = 0)
+    df = pd.concat([train, test], axis=0)
+
 
 def normalizeVar():
     df['HomePlanet'].replace('Earth', 0, inplace=True)
@@ -26,19 +31,21 @@ def normalizeVar():
     df['CryoSleep'].replace(False, 0, inplace=True)
     df['CryoSleep'].replace(True, 1, inplace=True)
 
+
 def NormalizeData():
-    passenger_cols = ('HomePlanet','CryoSleep','Cabin','Destination','VIP','Name','Transported')
+    passenger_cols = ('HomePlanet', 'CryoSleep', 'Cabin', 'Destination', 'VIP', 'Name', 'Transported')
 
     df[passenger_cols] = df[passenger_cols].apply(LabelEncoder().fit_transform())
     df.head()
 
+
 def visualizeData():
     init()
 
-    df.info(), print(df.head() )
+    df.info(), print(df.head())
 
-    print(" champ manquant : " )
-    print (df.isnull().sum())
+    print(" champ manquant : ")
+    print(df.isnull().sum())
 
     print(df['HomePlanet'].value_counts())
     print(df['HomePlanet'].isnull().sum())
@@ -51,5 +58,22 @@ def visualizeData():
     print(df['HomePlanet'].value_counts())
     print(df['CryoSleep'].value_counts())
 
-visualizeData()
+
+def standardScalerVariablesNumeriques():
+    standardScalerData = ['RoomService', 'FoodCourt', 'ShoppingMall', 'Spa', 'VRDeck']
+
+    dfStandardScalerData = df[standardScalerData]
+
+    for i in standardScalerData:
+        df[i] = df[i].fillna(0).astype(int)
+
+    print("dfStandardScalerData")
+    print(dfStandardScalerData)
+
+    scale = StandardScaler().fit_transform(dfStandardScalerData)
+    print(scale)
+
+# visualizeData()
 # NormalizeData()
+init()
+standardScalerVariablesNumeriques()
