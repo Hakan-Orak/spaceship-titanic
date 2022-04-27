9b676babf5abfd5839e40a940639779c48f00f24
Hakan Orak <hakan.orak@hotmail.fr>
2022-04-27 10:52:47 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import warnings
+import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.preprocessing import LabelEncoder, OrdinalEncoder
+
+def init():
+    global df,test_ID,train_ID
+
+    train = pd.read_csv('data/train.csv')
+    test = pd.read_csv('data/test.csv')
+
+    train.info(), test.info()
+
+    test_ID = test['PassengerId']
+    train_ID = train['PassengerId']
+    df = pd.concat([train,test],axis = 0)
+
+def normalizeVar():
+    df['HomePlanet'].replace('Earth', 0, inplace=True)
+    df['HomePlanet'].replace('Mars', 1, inplace=True)
+    df['HomePlanet'].replace('Europa', 2, inplace=True)
+
+    df['CryoSleep'].replace(False, 0, inplace=True)
+    df['CryoSleep'].replace(True, 1, inplace=True)
+
+def NormalizeData():
+    passenger_cols = ('HomePlanet','CryoSleep','Cabin','Destination','VIP','Name','Transported')
+
+    df[passenger_cols] = df[passenger_cols].apply(LabelEncoder().fit_transform())
+    df.head()
+
+def visualizeData():
+    init()
+
+    df.info(), print(df.head() )
+
+    print(" champ manquant : " )
+    print (df.isnull().sum())
+
+    print(df['HomePlanet'].value_counts())
+    print(df['HomePlanet'].isnull().sum())
+    print(df['CryoSleep'].value_counts())
+    print(df['Cabin'].isnull().sum())
+
+    print("\n\n variable aprés traitement")
+    normalizeVar()
+
+    print(df['HomePlanet'].value_counts())
+    print(df['CryoSleep'].value_counts())
+
+visualizeData()
+# NormalizeData()
